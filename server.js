6975d94d3510df359a4ff26af4a0c52dabc3fdde
Bcardoso2 <38514089+Bcardoso2@users.noreply.github.com>
2025-09-20 18:11:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js - Backend Node.js para AUTOGIRO com MySQL
 const express = require('express');
 const mysql = require('mysql2/promise');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
